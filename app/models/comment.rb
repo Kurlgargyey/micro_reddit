a92e2975ed389c8e2ed0fa6a3492c2df3399a7e3
Kurlgargyey <nahsirkivar@gmail.com>
2022-10-17 22:18:24 +0200

    Add comment association and validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ApplicationRecord
+  validates :body, :user, :post, presence: true
+
+  belongs_to :post
+  belongs_to :user
 end
--- db/migrate/20221017200920_create_comments.rb
+++ db/migrate/20221017200920_create_comments.rb
@@ -2,6 +2,8 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.text :body
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :post, foreign_key: true
 
       t.timestamps
     end
